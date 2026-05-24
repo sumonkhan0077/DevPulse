dc528bf7628dde4f87a34438bf2a87c89199d9ed
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-24 08:58:52 +0600

    added user and issuse table make

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,37 @@
 import { Pool } from "pg";
-import { config } from "../config/env"
+import { config } from "../config/env";
 
 export const pool = new Pool({
-    connectionString : config.connection_string,
-})
+  connectionString: config.connection_string,
+});
 
+export const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(80) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role VARCHAR(50) NOT NULL DEFAULT 'contributor' CHECK (role in ('contributor', 'maintainer')),
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        );
+    `);
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues (
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(150) NOT NULL,
+          description TEXT NOT NULL CHECK (LENGTH(description) >= 20),
+          type VARCHAR(50) NOT NULL CHECK (type in ('bug', 'feature_request')),
+          status VARCHAR(20) DEFAULT 'open' CHECK (status IN ('open','in_progress', 'resolved')),
+          reporter_id INT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    `);
+    console.log("Database connected and tables initialized successful!");
+  } catch (error) {
+     console.error("DB init error:", error);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app"
 import { config } from "./config/env"
+import { initDB } from "./db"
 
 const main = () => {
+    initDB()
     app.listen(config.port, () => {
         console.log(`server run on port ${config.port}`)
     })
