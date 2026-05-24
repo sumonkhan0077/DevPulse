7ec7ca934cba88f0f13e6e44574fdebefd12c1f5
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-24 11:03:26 +0600

    added issuse type add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { type Application, type Request, type Response } from "express"
 import { authRouter } from "./modules/auth/auth.routes";
+import { issuesRouter } from "./modules/issues/issues.routes";
 
 const app: Application = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/auth", authRouter);
+app.use("/api/issues", issuesRouter);
 
 
 export default app;
