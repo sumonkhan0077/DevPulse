235095ecdb1fdfc91e2fc50947c22202a0d0c506
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-05-24 08:18:29 +0600

    added db pool connect

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,6 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
-app.get("/", (req: Request, res: Response) => {
-  //res.send("Hello World!");
-  res.status(200).json({
-    message: "Express Server",
-    author: "Next Level",
-  });
-});
+
 
 export default app;
